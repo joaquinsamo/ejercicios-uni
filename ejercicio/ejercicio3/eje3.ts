dd83d099115ae84523f12d7370adaaab9b503400
joaquinsamo <joaquinsamo0101@gmail.com>
2026-04-08 15:05:36 -0300

    eje34

---
+++ ejercicio/ejercicio3/eje3.ts
@@ -0,0 +1,5 @@
+const numer1: number = 12345;
+const numer2: number = 67890;
+const resultad: number = numer1 + numer2;
+
+console.log(`La suma de ${numer1} y ${numer2} es igual a ${resultad}`);
---
+++ ejercicio/ejercicio4/producto.ts
@@ -0,0 +1,5 @@
+const number1: number = 12;
+const number2: number = 13;
+const producto: number = number1 * number2;
+
+console.log(`El producto de ${number1} y ${number2} es igual a ${producto}`);
